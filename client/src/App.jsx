ce9e7f394ff739e1382e500ccdec68c84b2e14b2
yad <ayyad2k2@gmail.com>
2024-01-11 17:56:57 +0800

    Laptop Repo, probs dont work on Desk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,24 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
-//components
-import InputToDo from "./components/InputTodo";
+// Components
+import InputToDo from "./components/InputToDo";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <>
-        <InputToDo />
-      </>
-
+    <div className="App">
       <div>
+        <InputToDo />
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1></h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -35,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const InputToDo = () => {
-  return <h1>Input ToDo</h1>;
+  return (
+    <>
+      <h1 className="mt-5">Lorem ipsum dolor sit.</h1>
+
+      <form action="" method="POST">
+        <input type="text" className="" />
+      </form>
+    </>
+  );
 };
 
 export default InputToDo;
